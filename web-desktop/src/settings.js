341e8d5a4f37f0fe43092815662716dcb5f69a58
uniquePaul <60089662+uniquePaul@users.noreply.github.com>
2020-07-21 21:12:02 -0700

    Add files via upload

---
+++ web-desktop/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/前提势要.fire",orientation:"",jsList:[]};
